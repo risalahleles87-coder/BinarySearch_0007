a336b738fd8a6b39bc0facc653998cb5cb9264fd
risalahlele87-coder <risalahleles87@gmail.com>
2026-04-16 15:26:35 +0700

    Implementasi algoritma binary search untuk pencarian elemen dalam array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -72,4 +72,35 @@ void display()
 
 void binarySearch()
 {
-    
+    char ulang;
+    do
+    {
+        cout << "\n=====================================\n";
+        cout << "    Pencarian Binary Search             \n";
+        cout << "=====================================\n";
+
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x;
+        
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+            
+            if (element[mid] == x)
+            {
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
+
